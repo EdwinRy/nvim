76a6943fd2c96bf18a5c568d735f419f41a294ba
Edwin Rybarczyk <edwinkry@gmail.com>
2023-02-03 12:16:36 +0000

    keep normal mode in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,17 @@
 require('telescope').setup {
-	defaults = {
-		mappings = {
-			i = {
-				["<C-o>"] = function(prompt_bufnr) require("telescope.actions").select_default(prompt_bufnr) require("telescope.builtin").resume() end,
-			}
-		}
-	}
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-o>"] = function(prompt_bufnr) require("telescope.actions").select_default(prompt_bufnr) require("telescope.builtin")
+            .resume() end,
+      },
+      n = {
+
+        ["o"] = function(prompt_bufnr) require("telescope.actions").select_default(prompt_bufnr) require("telescope.builtin")
+            .resume({initial_mode = "normal"}) end,
+      }
+    }
+  }
 }
 
 local builtin = require('telescope.builtin')
@@ -14,7 +20,7 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+  builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
 vim.keymap.set('n', '<leader>fl', builtin.oldfiles, {})
