8ea45d54aad2144fefb9bcc818f653e10be83879
Edwin <edwin.rybarczyk@liberis.com>
2024-07-01 22:42:59 +0100

    add dap

---
+++ lua/plugins/nvim-dap.lua
@@ -0,0 +1,10 @@
+return {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    cond = (function() return not vim.g.vscode end),
+    config = function()
+        require("neodev").setup({
+            library = { plugins = { "nvim-dap-ui" }, types = true },
+        })
+    end
+}
