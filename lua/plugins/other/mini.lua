30ba50e5977748562d2de2dd65888df0002477cc
Edwin <edwin.rybarczyk@liberis.com>
2024-09-05 03:32:38 +0100

    mini cond

--- lua/plugins/other/mini.lua
+++ lua/plugins/other/mini.lua
@@ -1,6 +1,7 @@
 return {
     "echasnovski/mini.nvim",
     version = "*",
+    cond = not vim.g.vscode,
     config = function()
         require("mini.splitjoin").setup({
             mappings = {
