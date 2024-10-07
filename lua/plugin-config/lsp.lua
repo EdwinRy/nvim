9f99304fc869dcbf045b9875e6ef04d1ccea14fe
Edwin <edwin.rybarczyk@liberis.com>
2024-10-07 04:49:12 +0100

    lspkind

--- lua/plugin-config/lsp.lua
+++ lua/plugin-config/lsp.lua
@@ -12,18 +12,21 @@ lsp_zero.configure("lua_ls", {
 
 local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local lspkind = require("lspkind")
+
+local lspKindExists, lspkind = pcall(require, "lspkind")
 
 cmp.setup({
     formatting = {
-        format = lspkind.cmp_format({
-            mode = "symbol_text", -- show only symbol annotations
-            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-            -- can also be a function to dynamically calculate max width such as
-            -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-            ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-            show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-        }),
+        format = lspKindExists
+                and lspkind.cmp_format({
+                    mode = "symbol_text", -- show only symbol annotations
+                    maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                    -- can also be a function to dynamically calculate max width such as
+                    -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+                    ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                    show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+                })
+            or nil,
     },
     mapping = cmp.mapping.preset.insert({
         ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
